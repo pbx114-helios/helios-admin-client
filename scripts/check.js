46d3140b5d4945cf632c5d61750c0c4c3b1c28f2
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2023-03-30 14:04:53 +0400

    Work on login integration

--- scripts/check.js
+++ scripts/check.js
@@ -1,5 +1,5 @@
 function check() {
-    if (document.referrer != 'https://pbx114-helios.github.io/helios-admin-client/index.html') {
+    if ((document.referrer != 'https://pbx114-helios.github.io/helios-admin-client/index.html') || (document.referrer != 'https://pbx114-helios.github.io/helios-admin-client/product.html') || (document.referrer != 'https://pbx114-helios.github.io/helios-admin-client/home.html')) {
         alert("Incorrect Credentials")
     }
 }

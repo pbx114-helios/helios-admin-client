cf4e186adfe1a50f9727f803e93ac8a6798b9dd0
amazinglysk <shashwat07ktm@gmail.com>
2023-04-01 22:35:44 +0400

    A lot of formatting and frustration. fixed cookies

--- scripts/addprod.js
+++ scripts/addprod.js
@@ -1,13 +1,28 @@
-var form = document.getElementById("newprod");
 function submitData() {
+    const token = sessionStorage.getItem("token")
+    console.log(token)
+    let form_data = {
+        name : $("#c-name").val(),
+        description : $("#c-desc").val(),
+        price : $("#c-price").val(),
+        token
+    }
     const url = "http://localhost:3000/products/"
     fetch(url, {
         method: "POST",
-        body: new FormData(form),
+        body: JSON.stringify(form_data),
+        headers : {
+            "Content-Type" : "application/json",
+        }
     })
         .then((response) => response.json())
         .then((jsonRes) => {
+            // Do what you wanna after a product is added
             alert(jsonRes.msg);
         });
 }
 
+$(".change").on('click', (e) => {
+    e.preventDefault()
+    submitData()
+})
--- scripts/login.js
+++ scripts/login.js
@@ -1,23 +1,25 @@
 function submit() {
-    let name = document.getElementById("username").value
-    let pwd = document.getElementById("password").value
-    const form_data = { password: pwd, username: name }
-    const url = "http://localhost:3000/admin/login"
+    let name = document.getElementById("username").value;
+    let pwd = document.getElementById("password").value;
+    const form_data = { password: pwd, username: name };
+    const url = "http://localhost:3000/admin/login";
     fetch(url, {
         method: "POST",
-        // credentials: "include",
         body: JSON.stringify(form_data),
         headers: {
-            'Content-Type': 'application/json'
-        }
+            "Content-Type": "application/json",
+            "SameSite" : "None"
+        },
     })
+        .then((res) => {
+            if (res.status == "200") {
+                return res.json();
+            }else{
+                alert("it shouldve worked");
+            }
+        })
         .then((response) => {
-            if (response.status == "200") {
-                // alert("logged in")
-                window.location.href = "./home.html"
-            }
-            else {
-                alert("it shouldve worked")
-            }
+            alert("logged in");
+            sessionStorage.setItem("token", response.token)
         });
 }
